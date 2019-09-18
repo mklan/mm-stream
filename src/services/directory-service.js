6a1b13b878560e3b414fffd85900910b76303bd1
Matthias Klan <matthias.klan@gmail.com>
2019-09-18 14:07:09 +0200

    Refactors dir service

--- src/services/directory-service.js
+++ src/services/directory-service.js
@@ -9,36 +9,34 @@ const map = util.promisify(async.map);
 const rootFolder = process.env.MM_FOLDER;
 
 const audioTypes = ["mp3", "flac", "ogg"];
+const isDirOrAudioFile = file =>
+  !file.isFile() || audioTypes.indexOf(file.name.split(".").slice(-1)[0]) > -1;
+
+const constructContentObject = (path, host) => async file => {
+  const isFile = file.isFile();
+  const currPath = path + "/" + file.name;
+
+  const { size } = await stat(currPath);
+  const sanitizedPath = encodeURIComponent(
+    currPath.replace(rootFolder + "/", "")
+  );
+
+  return {
+    size,
+    isFile,
+    name: file.name,
+    path: sanitizedPath,
+    enter: `http://${host}/${isFile ? "stream" : "list"}?path=${sanitizedPath}`
+    //children
+  };
+};
 
 async function getContent(path, host) {
   const files = await readdir(path, { withFileTypes: true });
-
-  const isDirOrAudioFile = file =>
-    !file.isFile() ||
-    audioTypes.indexOf(file.name.split(".").slice(-1)[0]) > -1;
-
-  const getDetails = async file => {
-    const isFile = file.isFile();
-    const currPath = path + "/" + file.name;
-
-    const { size } = await stat(currPath);
-    const sanitizedPath = encodeURIComponent(
-      currPath.replace(rootFolder + "/", "")
-    );
-
-    return {
-      size,
-      isFile,
-      name: file.name,
-      path: sanitizedPath,
-      enter: `http://${host}/${
-        isFile ? "stream" : "list"
-      }?path=${sanitizedPath}`
-      //children
-    };
-  };
-
-  return map(files.filter(isDirOrAudioFile), getDetails);
+  return map(
+    files.filter(isDirOrAudioFile),
+    constructContentObject(path, host)
+  );
 }
 
 module.exports = getContent;
